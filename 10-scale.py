e25ff759002d937d487e3d2344f1c629036d7d4f
Paul Ventura <j.paul.ventura@gmail.com>
2024-05-11 15:38:50 -0500

    10

--- 10-scale.py
+++ 10-scale.py
@@ -6,7 +6,17 @@ scale = StandardScaler()
 df = pandas.read_csv("data.csv")
 
 X = df[['Weight', 'Volume']]
+y = df['CO2']
 
 scaledX = scale.fit_transform(X)
 
 print(scaledX)
+
+# Predict CO2 values
+regr = linear_model.LinearRegression()
+regr.fit(scaledX, y)
+
+scaled = scale.transform([[2300, 1.3]]) # Predict the CO2 emission from a 1.3 liter car that weighs 2300 kilograms:
+
+predictedCO2 = regr.predict([scaled[0]])
+print(predictedCO2)
