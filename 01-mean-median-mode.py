60a2ea0b33aaf5ac8be573f847620c32c4cd211f
Paul Ventura <j.paul.ventura@gmail.com>
2024-05-11 14:42:29 -0500

    Mean median mode

--- 01-mean-median-mode.py
+++ 01-mean-median-mode.py
@@ -1,7 +1,14 @@
 import numpy
+# python -m pip install scipy
+from scipy import stats
 
 speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
 
 x = numpy.mean(speed)
+y = numpy.median(speed)
+z = stats.mode(speed)
 
 print(x)
+print(y)
+print(z)
+#print(z.mode[0]) #Return
